ebdaa5e9cce6d6852d80b4634624fb4c3cc6deae
zhangzheng <zhangzheng@youzan.com>
2018-01-21 12:41:56 +0800

    1 为adminservice通过filter的方式增加接口的访问日志，同时排除/health接口的访问日志 2 修改本地的端口号为8060，避免与shadowsocks冲突

---
+++ apollo-adminservice/src/main/java/com/ctrip/framework/apollo/adminservice/configuration/RequestLoggingFilterConfig.java
@@ -0,0 +1,28 @@
+package com.ctrip.framework.apollo.adminservice.configuration;
+
+import com.ctrip.framework.apollo.adminservice.filter.RequestLoggingFilterWithExcludedUris;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Create by zhangzheng on 2018/1/20
+ */
+@Configuration
+public class RequestLoggingFilterConfig {
+    @Bean
+    public FilterRegistrationBean logFilter() {
+
+        RequestLoggingFilterWithExcludedUris filter
+                = new RequestLoggingFilterWithExcludedUris();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.addExcludedUri("/health");
+
+        FilterRegistrationBean filterBean = new FilterRegistrationBean();
+        filterBean.setFilter(filter);
+        filterBean.addUrlPatterns("/*");
+        return filterBean;
+    }
+}
