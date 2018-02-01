33a6262bfc5d66bf9d531389cae76f92cf7a20ac
zhangzheng <zhangzheng@youzan.com>
2018-02-01 14:48:18 +0800

    1 增加线上的配置数据，数据库的地址以及eureka集群的地址 2 设置qa环境的configService默认开启缓存

--- scripts/sql/qa-env/apolloconfigdb.sql
+++ scripts/sql/qa-env/apolloconfigdb.sql
@@ -5,5 +5,5 @@ VALUES
     ('eureka.service.url', 'default', '', 'Eureka服务Url，多个service以英文逗号分隔'),
     ('namespace.lock.switch', 'default', 'false', '一次发布只能有一个人修改开关'),
     ('item.value.length.limit', 'default', '20000', 'item value最大长度限制'),
-    ('config-service.cache.enabled', 'default', 'false', 'ConfigService是否开启缓存，开启后能提高性能，但是会增大内存消耗！'),
+    ('config-service.cache.enabled', 'default', 'true', 'ConfigService是否开启缓存，开启后能提高性能，但是会增大内存消耗！'),
     ('item.key.length.limit', 'default', '128', 'item key 最大长度限制');
