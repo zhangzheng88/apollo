d88717adc793c1bd1678cd3d37891c0cf72bd2e0
zhangzheng <zhangzheng@youzan.com>
2018-02-06 10:59:07 +0800

    修改线上环境的默认配置为，一次配置的发布只能一个人修改，另一个人发布

--- scripts/sql/prod-env/apolloconfigdb.sql
+++ scripts/sql/prod-env/apolloconfigdb.sql
@@ -3,7 +3,7 @@
 INSERT INTO `ServerConfig` (`Key`, `Cluster`, `Value`, `Comment`)
 VALUES
     ('eureka.service.url', 'default', '', 'Eureka服务Url，多个service以英文逗号分隔'),
-    ('namespace.lock.switch', 'default', 'false', '一次发布只能有一个人修改开关'),
+    ('namespace.lock.switch', 'default', 'true', '一次发布只能一个人修改，另一个人发布'),
     ('item.value.length.limit', 'default', '20000', 'item value最大长度限制'),
     ('config-service.cache.enabled', 'default', 'true', 'ConfigService是否开启缓存，开启后能提高性能，但是会增大内存消耗！'),
     ('item.key.length.limit', 'default', '128', 'item key 最大长度限制');
